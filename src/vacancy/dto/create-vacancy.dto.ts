daeb97d9b25b2366f2f687ee473f2527a46afc91
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-02-14 15:53:30 +0700

    Changed dto for creating vacancy

--- src/vacancy/dto/create-vacancy.dto.ts
+++ src/vacancy/dto/create-vacancy.dto.ts
@@ -1,3 +1,4 @@
+import { City } from 'src/citys/citys.entity';
 import { ProfField } from 'src/prof-field/prof-field.entity';
 import { User } from 'src/users/users.entity';
 
@@ -9,7 +10,7 @@ export class CreateVacancyDto {
 
     readonly description: string;
 
-    readonly city_id: number;
+    readonly city: City;
 
-    readonly prof_fields: number[];
+    readonly prof_fields: ProfField[];
 }
