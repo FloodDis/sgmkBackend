81bfd21cf29f91d5d1b464d9b6742b04c34344cb
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-02-15 22:07:58 +0700

    Добавлены функции создания и получения всех регионов

--- src/regions/regions.controller.ts
+++ src/regions/regions.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateRegionDto } from './dto/create-region.dto';
+import { RegionsService } from './regions.service';
 
 @Controller('regions')
-export class RegionsController {}
+export class RegionsController {
+
+    constructor(private regionService: RegionsService) { }
+
+    @Get()
+    getAllRegions() {
+        return this.regionService.getAllRegions();
+    }
+
+    @Post()
+    createRegion(@Body() regionDto: CreateRegionDto) {
+        this.regionService.createRegion(regionDto);
+    }
+}
--- src/regions/regions.service.ts
+++ src/regions/regions.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRegionDto } from './dto/create-region.dto';
+import { RegionResponceDto } from './dto/region-responce.dto';
+import { Region } from './regions.entity';
 
 @Injectable()
-export class RegionsService {}
+export class RegionsService {
+    constructor(@InjectRepository(Region) private regionRepository: Repository<Region>) { }
+
+    async getAllRegions() {
+        const regions = this.regionRepository.find();
+
+        return (await regions).map((x) => new RegionResponceDto(x));
+    }
+
+    async createRegion(regionDto: CreateRegionDto) {
+        const region = new Region();
+        region.region_name = regionDto.region_name;
+        this.regionRepository.save(region);
+
+        return region;
+    }
+}
