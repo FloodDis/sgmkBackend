eb1de9cc33dc0052b07aa4aaed21ff7e1575f131
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-02-15 09:57:25 +0700

    entity для регионов подключена в модуль для регионов

--- src/regions/regions.module.ts
+++ src/regions/regions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RegionsController } from './regions.controller';
+import { Region } from './regions.entity';
 import { RegionsService } from './regions.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Region])],
   controllers: [RegionsController],
   providers: [RegionsService]
 })
-export class RegionsModule {}
+export class RegionsModule { }
