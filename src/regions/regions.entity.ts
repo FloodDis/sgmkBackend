3a6d0caebf5cc0599917f0c5f9b890b47b892afa
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-02-15 09:11:26 +0700

    Создана entity для регионов

---
+++ src/regions/regions.entity.ts
@@ -0,0 +1,16 @@
+import { City } from 'src/citys/citys.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Entity('regions')
+export class Region {
+
+    @PrimaryGeneratedColumn()
+    region_id: number;
+
+    @Column({ type: 'text' })
+    region_name: string;
+
+    @OneToMany(() => City, (city) => city.region)
+    cities: City[];
+}
