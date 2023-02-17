1cb5b4ff9c3a346c8ce5dd34cf58bba4fbec9022
Vlad Sechenov <vlad.sechenov@mail.ru>
2023-02-17 13:09:13 +0700

    Изменена dto для создания компании

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,3 +1,8 @@
+import { CreateVacancyDto } from 'src/vacancy/dto/create-vacancy.dto';
+
 export class CreateCompanyDto {
+
     readonly company_name: string;
+
+    readonly vacancy: CreateVacancyDto;
 }
